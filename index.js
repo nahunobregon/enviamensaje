03a388d1c3b402dd21c2fc7c3fc995b7b0d8a689
nahun obregon <nahunobregon@gmail.com>
2022-07-07 21:53:35 -0500

    a

---
+++ index.js
@@ -0,0 +1,28 @@
+const qrcode = require('qrcode-terminal');
+const readline = require('readline');
+const fs = require('fs')
+const { MessageMedia } = require('whatsapp-web.js');
+const { Client,LocalAuth  } = require('whatsapp-web.js');
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+client.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
+    //console.log(qr);
+});
+client.on('ready', () => {
+    function enviamensaje(){
+        console.log("envia mensaje");
+    }
+    console.log('Client is ready!');
+    const readInterface = readline.createInterface({
+        input: fs.createReadStream('./numeros.txt'),
+    });
+    readInterface.on('line', function(line) {
+        const number = "+51"+line;
+        const chatId = number.substring(1) + "@c.us";
+        const media = MessageMedia.fromFilePath('./Molduras-Vialci-Catalogo-acrilico.pdf');
+        client.sendMessage(chatId, media);
+    });
+});
+client.initialize();
